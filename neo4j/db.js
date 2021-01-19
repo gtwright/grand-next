d2fac812496a1d592364e482ff84b7717b8d7c9c
Graham <gtwright@gmail.com>
2021-01-18 20:25:21 -0500

    add example

--- neo4j/db.js
+++ neo4j/db.js
@@ -1,21 +1,11 @@
 import * as neo4j from "neo4j-driver";
 
-// Neo4j Desktop settings
-export const DEFAULT_NEO4J_DESKTOP = {
-  URI: "bolt://localhost:7687",
-  USER: "neo4j",
-  PASSWORD: "letmein",
-  isEncrypted: true, // v3.5 does not use encrypted connections, but 4.0 does
-};
-
-// Create a configured neo4j driver instance (this doesn't start a session)
 export const driver = (
   neo4jURI = process.env.NEO4J_URI,
   neo4jUser = process.env.NEO4J_USER,
   neo4jPassword = process.env.NEO4J_PASSWORD,
   neo4jEncryptedConnection = process.env.NEO4J_ENCRYPTED
 ) => {
-  // REMEMBER: !!('false') IS true; we need to explicitly check for a false string value
   const isEncrypted =
     !!neo4jEncryptedConnection && neo4jEncryptedConnection != "false";
 
