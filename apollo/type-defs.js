9ba087fcf4d4e2ae3560757c2e0c23b9b999b8e7
Graham <gtwright@gmail.com>
2021-01-18 20:23:46 -0500

    make augmented schema

--- apollo/type-defs.js
+++ apollo/type-defs.js
@@ -3,9 +3,19 @@
 export const typeDefs = `
 type Movie {
     title: String
-    year: Int
-    imdbRating: Float
+    released: Int
+    id: ID!
+    released: Date
     genres: [Genre] @relationship(type: "IN_GENRE", direction: "OUT")
+    actors: [Person] @relationship(type: "ACTED_IN", direction: "IN")
+
+}
+
+type Person {
+  id: ID!
+  born: Int
+  name: String!
+  acted_in: [Movie] @relationship(type: "ACTED_IN", direction: "OUT")
 }
 
 type Genre {
